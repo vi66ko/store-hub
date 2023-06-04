c0111dcc1f49a77e3b5437d2470f40fad8d10f95
V.Stamenov <vi66ko@mail.bg>
2023-06-05 00:50:47 +0100

    interface for the specific tables & field in the DB

---
+++ src/lib/server/db/tables_Interface.js
@@ -0,0 +1,58 @@
+/** @typedef { "users" | "sels" | "log"} Tables */
+
+/**
+ * Fields for the 'user' table
+ * 
+ * @typedef {object} User
+ * @property {string} [email_primary_key]
+ * @property {string} [username]
+ * @property {string} [password]
+ * @property {string} [settings] Could be JSON but for now is optional
+ */
+
+/**
+ * Fields for the 'products' table.
+ * 
+ * @typedef {object} Products
+ * @property {string} [id_primary_key]
+ * @property {string} [barcode]
+ * @property {string} [name]
+ * @property {number} [price]
+ * @property {string} [category]
+ * @property {number} [quantity]
+ */
+
+/**
+ * Fields for the 'sells' table.
+ * 
+ * @typedef {object} Sells
+ * @property {string} [product_foreign_key] 
+ * @property {string} [seller_foreign_key] 
+ * @property {string} [price] - The price of the moment of sell
+ * @property {Date} [date]
+ */
+
+
+
+
+let User = /**@type {User} */ ({})
+
+/**@type {Sells} */
+let Sells;
+
+/**@type {Tables} */
+let Table;
+
+
+
+export { Table, User, Sells };
+
+
+// Another options is to use enumerator
+
+// const tables = {
+//     TABLE_ONE: "user",
+//     TABLE_TWO: "sels",
+//     TABLE_THREE: "log",
+//     TABLE_FOUR: "opaa",
+// };
