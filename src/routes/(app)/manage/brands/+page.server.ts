b3eb05dcf2f348105add8836187b8249b1052aef
V.Stamenov <vi66ko@mail.bg>
2023-07-20 23:49:18 +0100

    generating table and adding new brad name

--- src/routes/(app)/manage/brands/+page.server.ts
+++ src/routes/(app)/manage/brands/+page.server.ts
@@ -1,5 +1,24 @@
-import type { PageServerLoad } from './$types';
+import db from '$lib/server/db/connectdb';
+import { fail } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
 
 export const load = (async () => {
-	return {};
+	const brands = await db.brands.getAll();
+
+	return { brands };
 }) satisfies PageServerLoad;
+
+export const actions = {
+	add: async ({ request }) => {
+		const formData = await request.formData();
+		const brandName = formData.get('name');
+
+		const result = await db.brands.add(brandName).catch((error) => error);
+
+		if (!result.isSuccessful) {
+			return fail(409, { existMessage: result.message });
+		}
+
+		return { success: true };
+	}
+} satisfies Actions;
--- src/routes/(app)/manage/brands/+page.svelte
+++ src/routes/(app)/manage/brands/+page.svelte
@@ -1,5 +1,62 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
+	import type { PageData, ActionData } from './$types';
 
 	export let data: PageData;
+	export let form: ActionData;
+	$: console.log(data);
+	$: console.log(form);
 </script>
+
+<!-- Responsive Container (recommended) -->
+
+<form
+	method="POST"
+	action="?/add"
+	use:enhance
+	class="w-[26rem] mx-auto flex items-end justify-between py-4"
+>
+	<label class="label">
+		<input
+			type="text"
+			placeholder="Brand name"
+			name="name"
+			required
+			class="input variant-form-material"
+		/>
+	</label>
+	<button class="h-11 btn variant-filled-secondary">+ add new brand</button>
+</form>
+{#if form?.existMessage}
+	<p>{form.existMessage}</p>
+{/if}
+
+<div class="mt-8 table-container">
+	<!-- Native Table Element -->
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Position</th>
+				<th>Name</th>
+				<th />
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.brands as row, i}
+				<tr>
+					<td class="w-16">{i + 1}</td>
+					<td>
+						{row.name}
+					</td>
+					<td />
+				</tr>
+			{/each}
+		</tbody>
+		<tfoot>
+			<tr>
+				<!-- <th colspan="3">Calculated Total Weight</th> -->
+				<!-- <td>{totalWeight}</td> -->
+			</tr>
+		</tfoot>
+	</table>
+</div>
