d05e358816d97a657df3cdd5873a7b19c2829dd7
V.Stamenov <vi66ko@mail.bg>
2023-07-23 15:01:18 +0100

    setUp front & backend +endpoints

--- src/routes/(app)/manage/categories/+page.server.ts
+++ src/routes/(app)/manage/categories/+page.server.ts
@@ -1,5 +1,52 @@
-import type { PageServerLoad } from './$types';
+import { error, fail } from '@sveltejs/kit';
+import db from '$lib/server/db/connectdb';
+import { spanWrap } from '$lib/gadgetBag';
+import type { PageServerLoad, Actions } from './$types';
 
 export const load = (async () => {
-	return {};
+	const categories = await db.categories.getdAll();
+
+	return { categories };
 }) satisfies PageServerLoad;
+
+export const actions = {
+	async add({ request }) {
+		const formData = await request.formData();
+		const name = formData.get('name');
+
+		const added = await db.categories.add(name).catch((error) => error);
+
+		if (added.error) {
+			return fail(409, { message: added.message });
+		}
+
+		return { success: true, message: '' };
+	},
+	async edit({ request }) {
+		const formData = await request.formData();
+		const categoryName = formData.get('name');
+		const newCategoryName = formData.get('newName');
+
+		if (categoryName === newCategoryName) {
+			return fail(409, { success: false, message: 'Same name' });
+		}
+
+		const updated = await db.categories
+			.update(categoryName, newCategoryName)
+			.catch((error) => error);
+
+		if (!updated) {
+			console.log('Something went wrong when trying to delete the category name');
+			console.log({ updated });
+			return fail(500, { success: false, message: 'Something went wrong.' });
+		}
+
+		return {
+			success: true,
+			message: `Successfully changed from ${spanWrap(categoryName)} to ${spanWrap(newCategoryName)}`
+		};
+	},
+	async delete({ request }) {
+		//
+	}
+} satisfies Actions;
--- src/routes/(app)/manage/categories/+page.svelte
+++ src/routes/(app)/manage/categories/+page.svelte
@@ -1,7 +1,163 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
+
+	import FaRegEdit from 'svelte-icons/fa/FaRegEdit.svelte';
+	import MdDelete from 'svelte-icons/md/MdDelete.svelte';
+
+	import { modalStore } from '@skeletonlabs/skeleton';
+
+	import { spanWrap, toast } from '$lib/gadgetBag';
+
+	import type { PageData, ActionData } from './$types';
+	import type { ModalSettings } from '@skeletonlabs/skeleton';
 
 	export let data: PageData;
+	export let form: ActionData;
+	$: console.log(data);
+
+	/**
+	 *
+	 * @param value
+	 * @returns {Promise<string>} An empty string or “value” string.
+	 */
+	function editFun(value: string): Promise<string> {
+		return new Promise((resolve) => {
+			const editModal: ModalSettings = {
+				type: 'prompt',
+				title: 'Edit',
+				body: '',
+				value: value,
+				valueAttr: { type: 'text', required: true, class: 'input variant-from-material' },
+				response: (r) => (r ? resolve(r) : resolve(''))
+			};
+			modalStore.trigger(editModal);
+		});
+	}
+
+	$: if (!form?.success && form?.message) {
+		toast.warning(form.message);
+	}
+	$: if (form?.success) {
+		toast.success(form.message);
+	}
 </script>
 
-<h1>Categories</h1>
+<form
+	method="POST"
+	action="?/add"
+	use:enhance
+	class="w-[36rem] mx-auto flex items-end justify-between py-4"
+>
+	<label class="label">
+		<input
+			type="text"
+			placeholder="Category name"
+			name="name"
+			required
+			autofocus
+			class="w-96 input variant-form-material"
+		/>
+	</label>
+	<button class="h-[42px] btn variant-filled-secondary">+ add new category</button>
+</form>
+
+<div class="h-96 overflow-y-auto mt-8 table-container">
+	<header class="flex sticky top-0 z-30 py-4 px-3 dark:bg-surface-700 font-bold">
+		<h3 class="mr-6">Position</h3>
+		<h3>Name</h3>
+	</header>
+	{#if data?.categories.length === 0}
+		<p class="px-2 py-4 text-center dark:bg-surface-800">
+			There is not any category records. Feel free to add.
+		</p>
+	{:else}
+		<table class="table table-hover rounded-t-none">
+			<!-- I do swap the table header with separate so I can make it stick to the top  -->
+			<!-- <thead>
+			<tr class="sticky top-0">
+				<th>Position</th>
+				<th>Name</th>
+				<th />
+			</tr>
+		</thead> -->
+			<tbody>
+				{#each data.categories as row, i}
+					<tr>
+						<td class="w-[5.25rem] text-center">{i + 1}</td>
+						<td>
+							{row.name}
+						</td>
+						<td class="flex justify-end">
+							<form
+								method="POST"
+								action="?/edit"
+								use:enhance={async ({ formData, cancel }) => {
+									const newName = await editFun(row.name);
+
+									if (newName.length === 0) {
+										toast.warning('The field should not be empty.');
+										cancel();
+										return;
+									}
+
+									const doesExist = data.categories.some(
+										(category) => category.name.toLowerCase() === newName.toLowerCase()
+									);
+
+									if (doesExist) {
+										toast.warning(`${spanWrap(newName)} already exist.`);
+										cancel();
+										return;
+									}
+
+									formData.set('newName', newName);
+
+									return ({ update }) => {
+										update();
+									};
+								}}
+							>
+								<input type="hidden" name="name" value={row.name} />
+								<button class="btn mr-4 variant-ringed-success">
+									<span class="w-4"><FaRegEdit /></span>
+									<span class="hidden md:inline font-light"> edit </span>
+								</button>
+							</form>
+							<form
+								method="POST"
+								action="?/delete"
+								use:enhance={async ({ submitter }) => {
+									submitter.disabled = true;
+
+									return ({ update }) => {
+										update();
+										submitter.disabled = false;
+									};
+								}}
+							>
+								<input type="hidden" name="name" value={row.name} />
+								<button class="btn variant-ringed-error">
+									<div class="w-4"><MdDelete /></div>
+									<span class="hidden md:inline font-light">delete</span>
+								</button>
+							</form>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+			<tfoot>
+				<tr>
+					<!-- <th colspan="3">Calculated Total Weight</th> -->
+					<!-- <td>{totalWeight}</td> -->
+				</tr>
+			</tfoot>
+		</table>
+	{/if}
+</div>
+
+<style>
+	td {
+		padding: 0.5rem !important;
+		vertical-align: baseline !important;
+	}
+</style>
