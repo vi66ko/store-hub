c7b35e29b5707b3494ea576e9cbd759c92d02c5d
V.Stamenov <vi66ko@mail.bg>
2023-07-23 15:02:48 +0100

    rout protection

---
+++ src/routes/(app)/home/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from './$types';
+
+import { redirect } from '@sveltejs/kit';
+
+export const load = (async ({ locals }) => {
+	if (!locals.user) throw redirect(302, '/');
+
+	return { locals };
+}) satisfies PageServerLoad;
