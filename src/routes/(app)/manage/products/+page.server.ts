53618e8eedb64060ff946bbcda56d3f51b2989c1
V.Stamenov <vi66ko@mail.bg>
2023-07-25 04:32:24 +0100

    handling CRUD

--- src/routes/(app)/manage/products/+page.server.ts
+++ src/routes/(app)/manage/products/+page.server.ts
@@ -1,26 +1,101 @@
+import db from '$lib/server/db/connectDb';
+import { error, fail } from '@sveltejs/kit';
+import { spanWrap } from '$lib/gadgetBag';
+
 import type { PageServerLoad, Actions } from './$types';
 
 export const load = (async () => {
-	const sourceData = [
-		{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-		{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-		{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-		{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' }
-	];
+	const products = await db.products.getAll();
+	const categories = await db.categories.getAll();
+	const brands = await db.brands.getAll();
 
-	return { sourceData };
+	return { products, categories, brands };
 }) satisfies PageServerLoad;
 
 export const actions = {
 	search: async () => {
 		console.log('Action activated');
 	},
-	add: async () => {},
-	edith: async () => {
-		console.log('editiiiiiiiiiing');
+	add: async ({ request }) => {
+		const formData = await request.formData();
+		const name: string = formData.get('name');
+		const description: string = formData.get('description');
+		const barcode: string = formData.get('barcode');
+		const price: string = formData.get('price');
+		const category: string = formData.get('category');
+		const brand: string = formData.get('brand');
+
+		if (name.length === 0)
+			return fail(400, { success: false, message: `The name field cannot be empty` });
+
+		if (price.length === 0)
+			return fail(400, { success: false, message: `The price field cannot be empty` });
+
+		const doesExist = await db.products.find(name).catch((error) => error);
+		if (doesExist) {
+			return fail(409, { success: false, message: `${spanWrap(name)} already exist.` });
+		}
+
+		const added = await db.products
+			.add(name, description, barcode, price, category, brand)
+			.catch((error) => error);
+
+		if (!added) {
+			return fail(409, {
+				success: false,
+				message: `Something went wrong when adding ${spanWrap(name)}.`
+			});
+		}
+
+		return {
+			success: true,
+			message: `Successfully add ${spanWrap(name)}`
+		};
 	},
-	delete: async () => {
-		console.log('333222xxxxxxxxxxxxxxxxxxxx');
+	edit: async ({ request }) => {
+		console.log(`[[[[[[[[[[[ Edit ]]]]]]]]]]]`);
+		const formData = await request.formData();
+		const id = formData.get('id');
+		const name = formData.get('name');
+		const description = formData.get('description');
+		const barcode = formData.get('barcode');
+		const price = formData.get('price');
+		const category = formData.get('category');
+		const brand = formData.get('brand');
+
+		const doesExist = await db.products.find(name, id).catch((error) => error);
+
+		if (doesExist) {
+			return fail(409, { success: false, message: `${spanWrap(name)} already exist.` });
+		}
+
+		const updated = await db.products
+			.update(id, name, description, barcode, price, category, brand)
+			.catch((error) => error);
+
+		if (!updated) {
+			console.log(`Something went wrong when trying to update ${name}`);
+			console.log({ updated });
+			return fail(500, { success: false, message: 'Something went wrong.' });
+		}
+
+		return {
+			success: true,
+			message: `Successfully updated ${spanWrap(name)}`
+		};
+	},
+	delete: async ({ request }) => {
+		const formData = await request.formData();
+		const name = formData.get('name');
+
+		const deleted = await db.products.delete(name).catch((error) => error);
+
+		if (!deleted) {
+			console.log(`Something went wrong when trying to delete the product with name = ${name}`);
+			console.log({ deleted });
+			return fail(500, { success: false, message: 'Something went wrong.' });
+		}
+
+		return { success: true, message: `Successfully deleted ${spanWrap(name)}` };
 	}
 } satisfies Actions;
